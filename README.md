8bcbcf2f89d0baefba541fefd80b7e4b66bf15e0
Shiiyo <camaille13@hotmail.fr>
2020-01-21 11:07:02 +0100

    Create the entity

---
+++ README.md
@@ -0,0 +1,9 @@
+# Projet6_SnowTricks
+OpenClassrooms project as part of the PHP / Symfony application developer courses.
+Using PHP 7.2.10 and Symfony 5.0.2
+
+##Install project
+Run the command line: <br/>
+<code>composer install</code><br/>
+Config your database on the .env file and run:<br/>
+<code>php bin/console doctrine:migrations:migrate</code><br/>
---
+++ src/Entity/Picture.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\PictureRepository")
+ */
+class Picture
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToMany(targetEntity="App\Entity\Trick", inversedBy="pictures")
+     */
+    private $trick;
+
+    /**
+     * @ORM\OneToOne(targetEntity="App\Entity\User", inversedBy="picture", cascade={"persist", "remove"})
+     */
+    private $user;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    public function __construct()
+    {
+        $this->trick = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return Collection|Trick[]
+     */
+    public function getTrick(): Collection
+    {
+        return $this->trick;
+    }
+
+    public function addTrick(Trick $trick): self
+    {
+        if (!$this->trick->contains($trick)) {
+            $this->trick[] = $trick;
+        }
+
+        return $this;
+    }
+
+    public function removeTrick(Trick $trick): self
+    {
+        if ($this->trick->contains($trick)) {
+            $this->trick->removeElement($trick);
+        }
+
+        return $this;
+    }
+
+    public function getUser(): ?User
+    {
+        return $this->user;
+    }
+
+    public function setUser(?User $user): self
+    {
+        $this->user = $user;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+}
