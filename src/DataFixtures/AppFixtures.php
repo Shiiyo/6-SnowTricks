0b0d7c6ddc6619df4fb4f4e1b8cc031c91521106
Shiiyo <camaille13@hotmail.fr>
2020-01-27 14:34:54 +0100

    Cut AppFixtures in multiple fixtures, one fixture by entity

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -15,57 +15,24 @@ class AppFixtures extends Fixture
     {
         $faker = \Faker\Factory::create('fr_FR');
 
-        //Create 20 User
-        $userArray = Array();
-        for($i = 1; $i <= 20; $i++){
-            $user = new User();
-            $user->setEmail($faker->email);
-            $user->setName($faker->lastName);
-            $user->setFirstName($faker->firstName);
-            $user->setPassword($faker->password);
-            $userArray[] = $user;
+        //Create Users
+        $userFixture = new UserFixtures();
+        $userFixture->load($manager);
 
-            $manager->persist($user);
-        }
-        //Create 8 trick_group
-        $trickGroupArray = Array();
-        for ($j = 1; $j <= 8; $j++) {
-            $trickGroup = new TrickGroup();
-            $trickGroup->setName($faker->word());
-            $trickGroupArray[] = $trickGroup;
+        //Create TrickGroups
+        $trickGroupFixture = new TrickGroupFixtures();
+        $trickGroupFixture->load($manager);
 
-            $manager->persist($trickGroup);
-        }
+        $manager->flush();
 
-        for ($k = 1; $k <= 10; $k++) {
-            $trick = new Trick();
-            $trick->setName($faker->words(2, true));
-            $trick->setContent($faker->paragraph);
-            shuffle($userArray);
-            $trick->setUser($userArray[0]);
-            $trick->setCreatedAt($faker->dateTimeBetween('-3 month', 'now'));
-            shuffle($trickGroupArray);
-            $trickGroup = $trickGroupArray[0];
-            $trick->addTrickGroup($trickGroup);
-            $trickGroup->addTrick($trick);
+        //Create tricks
+        $trickFixture = new TrickFixtures();
+        $trickFixture->load($manager);
+        $manager->flush();
 
-            $manager->persist($trick);
-            $manager->persist($trickGroup);
-
-            $now = new \DateTime();
-            $interval = $now->diff($trick->getCreatedAt())->days;
-
-            for ($l = 1; $l <= random_int(3,6); $l++){
-                $comment = new Comment();
-                $comment->setTrick($trick);
-                $comment->setCreatedAt($faker->dateTimeBetween('-'.$interval.' days'));
-                shuffle($userArray);
-                $comment->setUser($userArray[0]);
-                $comment->setContent($faker->sentences(random_int(2,6), true));
-
-                $manager->persist($comment);
-            }
-        }
+        //Create comment for each trick
+        $commentFixtures = new CommentFixtures();
+        $commentFixtures->load($manager);
 
         $manager->flush();
     }
---
+++ src/DataFixtures/TrickFixtures.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Trick;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class TrickFixtures extends Fixture
+{
+    /**
+     * @param ObjectManager $manager
+     */
+    public function load(ObjectManager $manager)
+    {
+        $faker = \Faker\Factory::create('fr_FR');
+
+        $userArray = $manager->getRepository('App\Entity\User')->findAll();
+        $trickGroupArray = $manager->getRepository('App\Entity\TrickGroup')->findAll();
+
+        //Create tricks and comment in each trick
+        for ($k = 1; $k <= 10; $k++) {
+            $trick = new Trick();
+            $trick->setName($faker->words(2, true));
+            $trick->setContent($faker->paragraph);
+            shuffle($userArray);
+            $trick->setUser($userArray[0]);
+            $trick->setCreatedAt($faker->dateTimeBetween('-3 month', 'now'));
+            shuffle($trickGroupArray);
+            $trickGroup = $trickGroupArray[0];
+            $trick->addTrickGroup($trickGroup);
+            $trickGroup->addTrick($trick);
+
+            $manager->persist($trick);
+            $manager->persist($trickGroup);
+
+        }
+    }
+}
