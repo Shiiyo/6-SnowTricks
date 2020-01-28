49a386565f46b4962f518ab07e5de50b67747a13
Shiiyo <camaille13@hotmail.fr>
2020-01-28 16:00:05 +0100

    Test all the entity

---
+++ tests/Entity/UserTest.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Tests;
+
+use App\Entity\User;
+use PHPUnit\Framework\TestCase;
+
+class UserTest extends TestCase
+{
+    private $user;
+
+    public function __construct($name = null, array $data = [], $dataName = '')
+    {
+        parent::__construct($name, $data, $dataName);
+        $this->user = new User();
+    }
+
+    public function testEmail()
+    {
+        $this->user->setEmail('test@test.fr');
+        $this->assertEquals('test@test.fr', $this->user->getEmail());
+    }
+
+    public function testName()
+    {
+        $this->user->setName('Test');
+        $this->assertEquals('Test', $this->user->getName());
+    }
+
+    public function testFirstName()
+    {
+        $this->user->setFirstName('Test');
+        $this->assertEquals('Test', $this->user->getFirstName());
+    }
+
+    public function testPassword()
+    {
+        $this->user->setPassword('Test');
+        $this->assertEquals('Test', $this->user->getPassword());
+    }
+}
