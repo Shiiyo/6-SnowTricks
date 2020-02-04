1d41bc4c04f033c1b9b6837ab9114f3428ef1201
Shiiyo <camaille13@hotmail.fr>
2020-02-04 14:41:28 +0100

    Show a trick with its id

---
+++ src/Controller/TrickController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Trick;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class TrickController extends AbstractController
+{
+    /**
+     * @Route("/trick/{id}", name="trick")
+     */
+    public function index(Trick $trick)
+    {
+        return $this->render('trick.html.twig', [
+            'trick' => $trick
+        ]);
+    }
+}
--- tests/Controller/HomeControllerTest.php
+++ tests/Controller/HomeControllerTest.php
@@ -9,7 +9,7 @@ class HomeControllerTest extends WebTestCase
     public function testIndex()
     {
         $client = static::createClient();
-        $crawler = $client->request('GET', '/');
+        $client->request('GET', '/');
         $this->assertEquals(200, $client->getResponse()->getStatusCode());
         $this->assertSelectorTextContains('html h1', 'Snowtricks');
         $this->assertSelectorTextContains('html h2', 'Tremplin aux acrobates');
