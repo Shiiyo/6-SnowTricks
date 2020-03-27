9c650ce7380bdcdce2144c69dfd07868bf03bab5
Shiiyo <camaille13@hotmail.fr>
2020-03-27 11:24:32 +0100

    Change the homepage design and add top and down arrow for easy scroll

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,1 +1,28 @@
 import '../css/home.scss';
+
+$(document).ready(function() {
+    //Slow scrolling
+        $('a[href*="#"]').click(function (event) {
+            var target = $(this.hash);
+            event.preventDefault();
+            $('html, body').animate({
+                scrollTop: target.offset().top
+            }, 1000);
+        });
+
+    //Top arrow show in scroll
+    if ($(document).width() > 1439)
+    {
+        $(window).scroll(function()
+        {
+            if ($(this).scrollTop() >= 100)
+            {
+                $('#top-arrow').fadeIn(200);
+            }
+            else
+            {
+                $('#top-arrow').fadeOut(200);
+            }
+        });
+    }
+});
