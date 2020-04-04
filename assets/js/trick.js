b0f12255a81f202d0afd4fce90a231844d46f0f2
Shiiyo <camaille13@hotmail.fr>
2020-04-04 11:28:54 +0200

    Create the button on small screen to see or hide medias

--- assets/js/trick.js
+++ assets/js/trick.js
@@ -5,4 +5,22 @@ $(document).ready(function () {
         $(this).toggleClass('clic-image');
     });
 
+    /*-----------------------------------------------------------------------------------*/
+
+    /*  /* Show trick media in small screen */
+
+    /*-----------------------------------------------------------------------------------*/
+    $("#loadMedia").on('click', function (e) {
+        e.preventDefault();
+        $("div.load-media").removeClass('d-none');
+        $("#loadMedia").addClass('d-none');
+        $("#hideMedia").removeClass('d-none');
+    });
+    $("#hideMedia").on('click', function (e) {
+            e.preventDefault();
+            $("div.load-media").addClass('d-none');
+            $("#loadMedia").removeClass('d-none');
+            $("#hideMedia").addClass('d-none');
+        });
+
 });
