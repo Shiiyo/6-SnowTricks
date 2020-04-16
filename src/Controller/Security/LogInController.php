27762ede2abde9c53e549ece679aaa2bd0ca2de6
Shiiyo <camaille13@hotmail.fr>
2020-04-16 11:08:02 +0200

    Show authentification error

--- src/Controller/Security/LogInController.php
+++ src/Controller/Security/LogInController.php
@@ -3,15 +3,24 @@
 namespace App\Controller\Security;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LogInController extends AbstractController
 {
     /**
      * @Route("/login", name="log_in")
      */
-    public function index()
+    public function index(AuthenticationUtils $authenticationUtils): Response
     {
-        return $this->render('security/login.html.twig');
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+        return $this->render('security/login.html.twig', [
+            'last_username' => $lastUsername,
+            'error' => $error
+        ]);
     }
 }
